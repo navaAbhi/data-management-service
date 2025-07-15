0dfaf04174ad7bcdda5cb467c3fec3d83a766ddc
AbhinavJoe <abhinavjoshi102003@gmail.com>
2025-07-16 02:31:34 +0530

    updated docker commands

--- scripts/start-server.ps1
+++ scripts/start-server.ps1
@@ -1,1 +1,13 @@
-uvicorn main:app --host 0.0.0.0 --port 8000 --reload
+# scripts.ps1
+Write-Host "Starting Redis server..."
+Start-Process -NoNewWindow -FilePath "redis-server" 
+
+Start-Sleep -Seconds 2
+
+Write-Host "Starting Celery worker..."
+Start-Process -NoNewWindow -FilePath "powershell" -ArgumentList "celery -A src.celery_app.celery_app worker --loglevel=info"
+
+Start-Sleep -Seconds 2
+
+Write-Host "Starting FastAPI (Uvicorn)..."
+uvicorn src.main:app --host 0.0.0.0 --port 8000 --reload
